2562b949cbeafebbb9399d34f86a212cbc139232
Tim Wise <tim@leisurehound.com>
2021-08-27 13:54:34 -0700

    Depend on async only.

--- Package.swift
+++ Package.swift
@@ -8,10 +8,10 @@ let package = Package(
     ],
     dependencies: [
         // ⏱ Promises and reactive-streams in Swift built for high-performance and scalability.
-        //.package(url: "https://github.com/vapor/async.git", from: "1.0.0-rc"),
+        .package(url: "https://github.com/vapor/async.git", from: "1.0.0-rc"),
 
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
-        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc"),
+        //.package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc"),
     ],
     targets: [
         .target(name: "TCP", dependencies: ["Bits", "COperatingSystem", "Debugging"]),
